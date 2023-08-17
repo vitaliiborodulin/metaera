73641cb220a55fa7a0fb113106d74d5f2d36f07e
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-08-17 11:30:35 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,48 @@
 $(function() {
-    console.log(1);
+    var ss_custom = {
+    	"main_bar_sticky": "true"
+    };
+    
+    function animateNav() {
+    	var win_scroll = $(window).scrollTop(),
+    			win_height = $(window).height(),
+    			main_nav = $('.header'),
+    			main_nav_offset = '',
+    			scroll_by = 120;
+    
+    	main_nav_offset = $(main_nav).outerHeight();
+    
+    	if (win_scroll > scroll_by) {
+    			$(main_nav).addClass('sticky-nav');
+    			window.setTimeout(function() {
+    					$(main_nav).addClass('fix');
+    			}, 200);
+    	} else {
+    			$(main_nav).removeClass('sticky-nav fix');
+    	}
+    
+    }
+    
+    animateNav();
+    
+    $(window).on('load scroll', function() {
+    	animateNav();
+    });
+    const burger = $('.header__burger');
+    // const menu = $('.menu__wrapper');
+    
+    
+    burger.on('click', function(e) {
+        e.preventDefault();
+        burger.toggleClass('header__burger--close');
+        // menu.toggleClass('open');
+    
+    });
+    
+    $(document).keyup(function(e) {
+        if (e.key === "Escape" || e.keyCode === 27) {
+            // menu.removeClass('open');
+            burger.removeClass('header__burger--close');
+        }
+    });
 });
