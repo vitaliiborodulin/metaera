86c8ce7d94ac825c223384891074a35c1c9970cc
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-08-23 22:10:07 +0300

    add about page

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -7,7 +7,7 @@ function animateNav() {
 			win_height = $(window).height(),
 			main_nav = $('.header'),
 			main_nav_offset = '',
-			scroll_by = 120;
+			scroll_by = 100;
 
 	main_nav_offset = $(main_nav).outerHeight();
 
