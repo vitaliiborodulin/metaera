fec2590e5717d8588a8e166f1d54b4f625d8f684
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-08-23 17:27:23 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -45,5 +45,24 @@ $(function() {
             burger.removeClass('header__burger--close');
         }
     });
+    Fancybox.bind("[data-fancybox]", {
+      // Your custom options
+    });
+    const container = document.querySelector(".carousel__serv");
+    const options = { 
+    	enabled: true,
+      breakpoints: {
+        "(max-width: 768px)": {
+          enabled: false,
+        },
+      },
+    	// Dots: {
+      //   minCount: 2,
+      // },
+    	Navigation: false,
+    	// 'dragFree': true,
+    	// 'slidesPerPage': 3,
+    };
     
+    new Carousel(container, options);
 });
