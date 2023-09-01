fc28086215d7d18a2ea707fc56e0d3fcc2bebf9f
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-09-01 09:14:42 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -106,7 +106,9 @@ $(function() {
     	// 'slidesPerPage': 3,
     };
     
-    if(servContainer) new Carousel(servContainer, servOptions);
+    if(servContainer){
+      const servCarousel = new Carousel(servContainer, servOptions)
+    };
     
     // service page
     
@@ -126,7 +128,7 @@ $(function() {
     };
     
     if(tabsSliderContainer) {
-      var tabsSliderCarousel = new Carousel(tabsSliderContainer, tabsSliderOptions);
+      const tabsSliderCarousel = new Carousel(tabsSliderContainer, tabsSliderOptions);
     
       $(window).resize(function(){
         tabsSliderCarousel.reInit();
@@ -147,4 +149,78 @@ $(function() {
     	$('.tabs__right-slider .f-carousel__slide[data-id='+id+']').slideDown(200);
     
     });
+    $.urlParam = function(name) {
+    	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    	if (results == null) {
+    			return null;
+    	} else {
+    			return decodeURI(results[1]) || 0;
+    	}
+    }
+    
+    function getCookie(c_name) {
+    	var i, x, y, ARRcookies = document.cookie.split(";");
+    	for (i = 0; i < ARRcookies.length; i++) {
+    			x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+    			y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+    			x = x.replace(/^\s+|\s+$/g, "");
+    			if (x == c_name) {
+    					return unescape(y);
+    			}
+    	}
+    }
+    
+    function createCookie(name, value, days) {
+    	var expires;
+    
+    	if (days) {
+    			var date = new Date();
+    			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    			expires = "; expires=" + date.toGMTString();
+    	} else {
+    			expires = "";
+    	}
+    	document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+    }
+    
+    var utm_source_cookie = getCookie('utm_source');
+    // var utm_campaign_cookie = getCookie('utm_campaign');
+    var utm_source_get = $.urlParam('utm_source');
+    // var utm_campaign_get = $.urlParam('utm_campaign');
+    
+    if (!utm_source_cookie && utm_source_get) {
+    	createCookie('utm_source', utm_source_get, '365');
+    	// createCookie('utm_campaign', utm_campaign_get, '365');
+    }
+    
+    if (!utm_source_cookie && !utm_source_get) return;
+    
+    var utm_source = utm_source_cookie ? utm_source_cookie : utm_source_get;
+    // var utm_campaign = utm_campaign_cookie ? utm_campaign_cookie : utm_campaign_get;
+    // console.log(utm_source);
+    
+    var form_utm_source = $('#utm_source');
+    
+    if (utm_source) {
+    	form_utm_source.val(utm_source);
+    }
+
+    $("input[type=tel]").mask("+375 (99) 999-99-99");
+
 });
+
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    // if(event.detail.contactFormId=="224"){
+    // onclick=yaCounter26043108.reachGoal('formsend'); return true;
+    // }
+
+    $(".form form").hide();
+    $(".form .form__thank").show();
+
+}, false );
+
+// $("input[type=submit]").on('click', function(e){
+//     e.preventDefault();
+//     $(".form form").hide();
+//     $(".form .form__thank").show();
+// });
