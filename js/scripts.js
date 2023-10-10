04b46d7049d89635402e3d78048aa7efa5a1da81
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2023-10-10 11:23:03 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -89,55 +89,55 @@ $(function() {
     Fancybox.bind("[data-fancybox]", {
       // Your custom options
     });
-    const servContainer = document.querySelector(".carousel__serv");
-    const servOptions = { 
-    	enabled: true,
-      breakpoints: {
-        "(max-width: 768px)": {
-          enabled: false,
-        },
-      },
-    	// Dots: {
-      //   minCount: 2,
-      // },
-    	Navigation: false,
-    	// 'dragFree': true,
-    	// 'slidesPerPage': 3,
-    };
+    // const servContainer = document.querySelector(".carousel__serv");
+    // const servOptions = { 
+    // 	enabled: true,
+    //   breakpoints: {
+    //     "(max-width: 768px)": {
+    //       enabled: false,
+    //     },
+    //   },
     
-    if(servContainer){
-      const servCarousel = new Carousel(servContainer, servOptions)
-    };
+    // 	Navigation: false,
     
-    const cardsContainer = document.querySelector(".carousel__cards");
-    const cardsOptions = { 
-    	enabled: true,
-      breakpoints: {
-        "(min-width: 768px)": {
-          enabled: false,
-        },
-        "(max-width: 450px)": {
-          enabled: false,
-        },
-      },
-    	// Dots: {
-      //   minCount: 2,
-      // },
-    	Navigation: false,
-      Dots: false,
-    	// 'dragFree': true,
-    	// 'slidesPerPage': 3,
-    };
+    // };
     
-    if(cardsContainer){
-      const cardsCarousel = new Carousel(cardsContainer, cardsOptions)
-    };
+    // if(servContainer){
+    //   const servCarousel = new Carousel(servContainer, servOptions)
+    // };
+    
+    const cardsContainer = document.querySelectorAll(".carousel__cards");
+    
+    for (i = 0; i < cardsContainer.length; ++i) {
+      const cardsOptions = { 
+        enabled: true,
+        breakpoints: {
+          "(min-width: 800px)": {
+            enabled: false,
+          },
+          "(max-width: 450px)": {
+            enabled: false,
+          },
+        },
+        // Dots: {
+        //   minCount: 2,
+        // },
+        Navigation: false,
+        Dots: false,
+        // 'dragFree': true,
+        // 'slidesPerPage': 3,
+      };
+      
+      if(cardsContainer){
+        new Carousel(cardsContainer[i], cardsOptions)
+      };
+    }
     
     // service page
     
     const tabsSliderContainer = document.querySelector(".tabs__right-slider");
     const tabsSliderOptions = { 
-    	enabled: false,
+    	enabled: true,
       breakpoints: {
         "(max-width: 768px)": {
           enabled: true,
@@ -146,8 +146,8 @@ $(function() {
       // Dots: false,
     	Navigation: false,
     	// 'dragFree': true,
-    	'slidesPerPage': 1,
-      infinite: false,
+    	// 'slidesPerPage': 1,
+      // infinite: false,
     };
     
     if(tabsSliderContainer) {
@@ -173,8 +173,15 @@ $(function() {
     
     });
 
-    // $(".input-phone").mask("+375 (99) 999-99-99");
+    $(".input-phone").mask("+375 (99) 999-99-99");
 
+    if($(window).width() < 576){
+    
+    	$('.footer__title').on('click', function() {
+    		$(this).toggleClass('active').next('.footer__menu').slideToggle();
+    	});
+    
+    }
     $.urlParam = function(name) {
     	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     	if (results == null) {
